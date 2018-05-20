986d5a69cddb42ae3a5e50f0d04d7110e883e406
Newman Wu <xiongnan.wu@yale.edu>
2018-05-20 15:44:05 -0500

    yes

--- src/thread.c
+++ src/thread.c
@@ -1,5 +1,5 @@
-uint thread_spawn(void * entry, uint id, uint quota) {
-    uint i = kctxt_new(entry, id, quota);
+uint thread_spawn(void * entry) {
+    uint i = kctxt_new(entry);
     set_state(i, READY);
     enqueue(num_chan, i);
     return i;
@@ -8,7 +8,7 @@ uint thread_spawn(void * entry, uint id, uint quota) {
 void thread_wakeup(uint chan_index) {
     uint proc_index = dequeue(chan_index);
     if(proc_index != num_proc) {
-        set_state(proc_index, TSTATE_READY);
+        set_state(proc_index, READY);
         enqueue(num_chan, proc_index);
     }
 }
@@ -16,5 +16,5 @@ void thread_wakeup(uint chan_index) {
 void sched_init() {
     tdqueue_init();
     set_curid(0);
-    set_state(0, TSTATE_RUN);
+    set_state(0, RUN);
 }
