460e03744c0553c2dd05c91acdb0c57332b60f20
Newman Wu <xiongnan.wu@yale.edu>
2018-07-07 22:53:02 -0500

    partial revisions on chapter 4

--- src/ptable_getter_setter.c
+++ src/ptable_getter_setter.c
@@ -22,21 +22,21 @@ void set_PDEU(uint proc_index, uint pde_index, uint pi) {
     PTPool[proc_index][pde_index] = (char *)(pi * PAGE_SIZE + PT_PERM_PTU);
 }
 
-uint get_PTE(uint proc_index, uint pde_index, uint vadr) {
+uint get_PTE(uint proc_index, uint pde_index, uint pte_index) {
     uint offset = (uint)PTPool[proc_index][pde_index] - PT_PERM_PTU;
-    return *(uint*)(offset + 4 * vadr);
+    return *(uint*)(offset + 4 * pte_index);
 }
 
-void set_PTE(uint proc_index, uint pde_index, uint vadr, uint padr, uint perm) {
+void set_PTE(uint proc_index, uint pde_index, uint pte_index, uint padr, uint perm) {
     uint offset = (uint)PTPool[proc_index][pde_index] - PT_PERM_PTU;
-    *(uint*)(offset + 4 * vadr) = padr * PAGE_SIZE + perm;
+    *(uint*)(offset + 4 * pte_index) = padr * PAGE_SIZE + perm;
 }
 
-void rmv_PTE(uint proc_index, uint pde_index, uint vadr) {
+void rmv_PTE(uint proc_index, uint pde_index, uint pte_index) {
     uint offset = (uint)PTPool[proc_index][pde_index] - PT_PERM_PTU;
-    *(uint*)(offset + 4 * vadr) = 0;
+    *(uint*)(offset + 4 * pte_index) = 0;
 }
 
-void set_IDPTE(uint pde_index, uint vadr, uint perm) {
-    IDPMap[pde_index][vadr] = (pde_index * 1024 + vadr) * PAGE_SIZE + perm;
+void set_IDPTE(uint pde_index, uint pte_index, uint perm) {
+    IDPMap[pde_index][pte_index] = (pde_index * 1024 + pte_index) * PAGE_SIZE + perm;
 }
--- src/ptableop.c
+++ src/ptableop.c
@@ -1,37 +1,37 @@
 uint pt_read(uint proc_index, uint vaddr) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    uint pdx_entry = get_PDE(proc_index, pdx_index);
-    if (pdx_entry == 0)
+    uint pde_index = vaddr / (4096 * 1024);
+    uint pde_entry = get_PDE(proc_index, pde_index);
+    if (pde_entry == 0)
       return 0;
     else {
-      uint vaddrl = (vaddr / 4096) % 1024;
-      return get_PTE(proc_index, pdx_index, vaddrl);
+      uint pte_index = (vaddr / 4096) % 1024;
+      return get_PTE(proc_index, pde_index, pte_index);
     }
 }
 
 uint pt_read_pde(uint proc_index, uint vaddr) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    return get_PDE(proc_index, pdx_index);
+    uint pde_index = vaddr / (4096 * 1024);
+    return get_PDE(proc_index, pde_index);
 }
 
 void pt_insert_aux(uint proc_index, uint vaddr, uint paddr, uint perm) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    uint vaddrl = (vaddr / 4096) % 1024;
-    set_PTE(proc_index, pdx_index, vaddrl, paddr, perm);
+    uint pde_index = vaddr / (4096 * 1024);
+    uint pte_index = (vaddr / 4096) % 1024;
+    set_PTE(proc_index, pde_index, pte_index, paddr, perm);
 }
 
 void pt_insert_pde(uint proc_index, uint vaddr, uint pi) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    set_PDEU(proc_index, pdx_index, pi);
+    uint pde_index = vaddr / (4096 * 1024);
+    set_PDEU(proc_index, pde_index, pi);
 }
 
 void pt_rmv_aux(uint proc_index, uint vaddr) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    uint vaddrl = (vaddr / 4096) % 1024;
-    rmv_PTE(proc_index, pdx_index, vaddrl);
+    uint pde_index = vaddr / (4096 * 1024);
+    uint pte_index = (vaddr / 4096) % 1024;
+    rmv_PTE(proc_index, pde_index, pte_index);
 }
 
 void pt_rmv_pde(uint proc_index, uint vaddr) {
-    uint pdx_index = vaddr / (4096 * 1024);
-    rmv_PDE(proc_index, pdx_index);
+    uint pde_index = vaddr / (4096 * 1024);
+    rmv_PDE(proc_index, pde_index);
 }
